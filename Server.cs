bba31dbd7be9d62545dcdbed77583dc6b6bc940d
Alexandr <sanier20@mail.ru>
2022-07-29 10:37:53 +0300

    Добавьте файлы проекта.

---
+++ Server.cs
@@ -0,0 +1,26 @@
+﻿public static class Server
+{
+    private static int count;
+    private static ReaderWriterLock s_lock = new ReaderWriterLock();
+
+    public static int GetCount()
+    {
+        s_lock.AcquireReaderLock(Timeout.InfiniteTimeSpan);
+        try
+        {
+            return count;
+        }
+        finally
+        {
+            s_lock.ReleaseReaderLock();
+        }
+    }
+
+    public static void AddToCount(int value)
+    {
+        s_lock.AcquireWriterLock(Timeout.InfiniteTimeSpan);
+        count += value;
+        s_lock.ReleaseWriterLock();
+    }
+}
+
